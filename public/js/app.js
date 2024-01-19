135f16ca0b93f8c64e05b5df30b30bbd95eb7db2
FireGamerXD <amineg715@gmail.com>
2024-01-19 10:59:24 +0100

    Map + sec1 Watch Video

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,37 @@
-let navbarbutton = document.querySelector(".navbarbutton1")
-let modal = document.querySelector(".modal-content")
+let btnModal = document.querySelectorAll('button')
+let modalBody = document.querySelectorAll(".myModal")
 
-navbarbutton.addEventListener("click", () => {
-    
-})
+
+btnModal.forEach(element => {
+
+
+    element.addEventListener("click", (e) => {
+
+        let myBTN = e.target
+        let myData = myBTN.getAttribute("data-mf-target")
+
+        modalBody.forEach(element => {
+
+            console.log(element);
+
+            if (myData == element.getAttribute("id")) {
+
+                element.classList.toggle("d-none")
+                let myCloseBTN = element.querySelector(".close")
+
+
+                myCloseBTN.addEventListener("click", () => {
+                    element.classList.toggle("d-none")
+                })
+
+                
+                document.body.classList.toggle("overflow-y-hidden")
+            }
+
+        });
+
+
+    })
+
+
+});
